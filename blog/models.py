5374d5693f013949c628db2415f8909a2f1acf20
VikTor <offcre.vikt@gmail.com>
2018-03-13 17:16:38 +0300

    blog models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
 
-class blog(model.models):
-    # Crea
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
